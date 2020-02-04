157807f758c60b47bd477811cec67be6be000339
Your Name <you@example.com>
2020-02-03 18:48:46 -0800

    did homework

---
+++ main.js
@@ -0,0 +1,33 @@
+"use strict";
+
+/******* Functions & event handlers *******/
+
+function changeColor() {
+  const colorChangeEls = $('.color-change');
+
+  for (const el of colorChangeEls) {
+    //$(el).toggleClass('bounce');
+    $(el).css('color', 'red');
+  }
+}
+
+function validateNumber(evt) {
+  evt.preventDefault();
+
+  const numberInput = $('input[name="number"]');
+  const userNum = parseInt(numberInput.val(), 10);  // typecast to num
+
+  const formFeedback = $('#formFeedback');
+  if (userNum > 10) {
+    formFeedback.text('Please enter a smaller number');
+  } else {
+    formFeedback.text('You are good to go!');
+  }
+}
+
+
+/******* Attach event handlers *******/
+
+$('.color-changer').on('click', changeColor);
+$('.number-form').on('submit', validateNumber);
+
